b0154d69f93d86de8a927ce58b2e8b3b0f71c5cf
syorito-hatsuki <enderdron3@gmail.com>
2023-01-30 00:39:07 +0200

    Fixed file access

--- src/main/kotlin/dev/syoritohatsuki/plugins/Routing.kt
+++ src/main/kotlin/dev/syoritohatsuki/plugins/Routing.kt
@@ -1,20 +1,18 @@
 package dev.syoritohatsuki.plugins
 
-import io.ktor.http.*
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
-import io.ktor.server.http.content.*
 import io.ktor.server.application.*
-import java.net.URI
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
 
     routing {
-        get("{...}") {
-            call.respondText(URI.create("http://localhost:1541/static/index.html").toURL().readText(), ContentType.Text.Html)
-        }
-        static("/static") {
-            resources("static")
+        static("/") {
+            staticBasePackage = "static"
+            defaultResource("index.html")
+            resource("css/style.css")
+            resource("icons/curseforge.svg")
+            resource("icons/modrinth.svg")
         }
     }
 }
